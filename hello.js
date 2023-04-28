238dd3f879187f7ffd643cbca864f354b386c15c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-28 11:35:59 +0900

    파일 정리

--- hello.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { program } from "commander";
-
-program
-  .version("0.1.0")
-  .description('A simple CLI tool that prints "Hello, World!"')
-  .parse(process.argv);
-
-console.log("Hello, World!");
