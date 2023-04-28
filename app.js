c9615729514ea3f48f7d7734a089be0d35439581
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-28 10:45:55 +0900

    inquirer로 fs 접근해보기?

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 import inquirer from "inquirer";
 import { program } from "commander";
+import fs from "fs";
 
 program.version("0.1.0v");
 program.description("commander 실행 테스트");
 program.parse(process.argv);
 
-console.log("hello world");
+let title = "";
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "fileName",
+      message: "생성할 html 파일의 이름을 입력해주세요.",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "html 파일 내의 title 정보를 입력해주세요.",
+    },
+  ])
+  .then((answers) => {
+    console.log("입력된 값:", answers);
+    title = answers;
+  });
+
+console.log(title);
